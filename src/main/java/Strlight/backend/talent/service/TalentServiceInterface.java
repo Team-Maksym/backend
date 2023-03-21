baca3b344cf8ec5ff1df71502a0531025d398588
tetyaZyna <sv2408sv@gmail.com>
2023-03-21 21:40:03 +0100

    Create GET "/talents" endpoint

--- src/main/java/Strlight/backend/talent/service/TalentServiceInterface.java
+++ src/main/java/Strlight/backend/talent/service/TalentServiceInterface.java
@@ -1,9 +1,7 @@
 package Strlight.backend.talent.service;
 
 import Strlight.backend.talent.model.response.TalentPagePagination;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface TalentServiceInterface {
-    public TalentPagePagination talentPagination(int page, int size);
+    TalentPagePagination talentPagination(int page, int size);
 }
