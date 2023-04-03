4cb628f7a38665bf7380f3b5222769672738f7fa
KateySt <kateystetsenko@gmail.com>
2023-04-03 20:36:33 +0300

    Creating a PATCH endpoint

--- src/main/java/starlight/backend/talent/TalentController.java
+++ src/main/java/starlight/backend/talent/TalentController.java
@@ -5,14 +5,12 @@ import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import starlight.backend.talent.model.response.TalentFullInfo;
 import starlight.backend.talent.model.response.TalentPagePagination;
+import starlight.backend.talent.model.response.TalentProfile;
+import starlight.backend.talent.model.response.TalentSession;
 import starlight.backend.talent.service.TalentServiceInterface;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
@@ -23,8 +21,8 @@ public class TalentController {
     private TalentServiceInterface talentService;
 
     @GetMapping("/talents")
-    public TalentPagePagination pagination (@RequestParam(defaultValue = "0") @Min(0) int page,
-                                            @RequestParam(defaultValue = "10") @Positive int size) {
+    public TalentPagePagination pagination(@RequestParam(defaultValue = "0") @Min(0) int page,
+                                           @RequestParam(defaultValue = "10") @Positive int size) {
         return talentService.talentPagination(page, size);
     }
 
@@ -36,8 +34,8 @@ public class TalentController {
 
     @PreAuthorize("hasRole('TALENT')")
     @PatchMapping("/talents/{talent-id}")
-    public TalentProfile edit (@PathVariable(value = "talentId") Long talentId,
-                                     @RequestBody TalentSession talentSession) {
+    public TalentProfile edit(@PathVariable(value = "talentId") Long talentId,
+                              @RequestBody TalentSession talentSession) {
         return null;//talentService.editTalent(talentId, talentSession);
     }
 }
