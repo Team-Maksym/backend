2761263195588df628b97660e807bbe83326574b
KateySt <kateystetsenko@gmail.com>
2023-04-05 19:09:43 +0300

    add Talent FullInfo

--- src/main/java/starlight/backend/proof/ProofController.java
+++ src/main/java/starlight/backend/proof/ProofController.java
@@ -1,117 +1,13 @@
 package starlight.backend.proof;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import starlight.backend.proof.model.request.ProofAddRequest;
-import starlight.backend.proof.model.response.ProofPagePagination;
-import starlight.backend.proof.service.ProofServiceInterface;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 @Validated
 @RequestMapping("/api/v1")
-@Tag(name = "Proof", description = "Proof API")
 public class ProofController {
-    private ProofServiceInterface proofService;
-
-    @Operation(
-            summary = "Get all proofs",
-            description = "Get list of all proofs. The response is list of talent objects with fields 'id','title', 'description' and 'dateCreated'."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = ProofPagePagination.class
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "Not found",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = Exception.class
-                            )
-                    )
-            )
-    })
-    @GetMapping("/proofs")
-    public ProofPagePagination pagination(@RequestParam(defaultValue = "0") @Min(0) int page,
-                                          @RequestParam(defaultValue = "10") @Positive int size,
-                                          @RequestParam(defaultValue = "true") Boolean sortDate) {
-        return proofService.proofsPagination(page, size, sortDate);
-    }
-
-    @Operation(
-            summary = "Add proof in status draft",
-            description = " "
-    )
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "201",
-                    description = "Created",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = ResponseEntity.class
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "Not found",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = Exception.class
-                            )
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "403",
-                    description = "Unauthorized",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(
-                                    implementation = Exception.class
-                            )
-                    )
-            )
-    })
-    @PreAuthorize("hasRole('TALENT')")
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/talents/{talent-id}/proofs")
-    public ResponseEntity<?> addProofFullInfo(@PathVariable("talent-id") long talentId,
-                                               @RequestBody ProofAddRequest proofAddRequest,
-                                               Authentication auth) {
-        var proofId = proofService.validationProofAdded(talentId, proofAddRequest, auth);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{proof-id}")
-                .buildAndExpand(proofId)
-                .toUri();
-        return ResponseEntity.created(location).build();
-    }
 }
