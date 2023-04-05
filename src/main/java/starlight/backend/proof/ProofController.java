3989181e682844de6850fbb44b28bc08946b5f8d
KateySt <kateystetsenko@gmail.com>
2023-04-05 20:37:41 +0300

    Create endpoint for getting proofs

--- src/main/java/starlight/backend/proof/ProofController.java
+++ src/main/java/starlight/backend/proof/ProofController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import starlight.backend.proof.model.request.ProofAddRequest;
+import starlight.backend.proof.model.response.ProofPagePagination;
 import starlight.backend.proof.service.ProofServiceInterface;
 
 import java.net.URI;
@@ -27,6 +30,38 @@ import java.net.URI;
 @Tag(name = "Proof", description = "Proof API")
 public class ProofController {
     private ProofServiceInterface proofService;
+    @Operation(
+            summary = "Get all proofs",
+            description = "Get list of all proofs. The response is list of talent objects with fields 'id','title', 'description' and 'dateCreated'."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Success",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = ProofPagePagination.class
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = Exception.class
+                            )
+                    )
+            )
+    })
+    @GetMapping("/proofs")
+    public ProofPagePagination pagination(@RequestParam(defaultValue = "0") @Min(0) int page,
+                                          @RequestParam(defaultValue = "5") @Positive int size,
+                                          @RequestParam(defaultValue = "true") Boolean sortDate) {
+        return proofService.proofsPagination(page, size, sortDate);
+    }
 
     @Operation(
             summary = "Add proof in status draft",
