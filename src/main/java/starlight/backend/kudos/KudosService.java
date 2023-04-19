e82ba8b4591eaef3ae8463dd60aea3d88ec78c34
tetyaZyna <sv2408sv@gmail.com>
2023-04-19 15:50:23 +0200

    11.2
    Add Service

---
+++ src/main/java/starlight/backend/kudos/KudosService.java
@@ -0,0 +1,29 @@
+package starlight.backend.kudos;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import starlight.backend.kudos.model.entity.KudosEntity;
+import starlight.backend.proof.model.entity.ProofEntity;
+import starlight.backend.security.service.SecurityServiceInterface;
+
+@Service
+@AllArgsConstructor
+public class KudosService {
+    private SecurityServiceInterface securityService;
+    @PersistenceContext
+    private EntityManager em;
+
+    private boolean isItMyProof(long proofId, Authentication auth) {
+        var talentId = em.find(ProofEntity.class, proofId).getUser().getUserId();
+        return securityService.checkingLoggedAndToken(talentId, auth);
+    }
+
+    private boolean isProofAlreadyHaveKudosFromUser(long kudosId, Authentication auth) {
+        var ownerId = em.find(KudosEntity.class, kudosId).getOwner().getUserId();
+        return securityService.checkingLoggedAndToken(ownerId, auth);
+    }
+
+}
