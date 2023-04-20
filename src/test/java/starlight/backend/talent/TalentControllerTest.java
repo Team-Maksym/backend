cbdf7c436ce1c97ebc83c88ae4104bc090c490cb
KateySt <kateystetsenko@gmail.com>
2023-04-20 11:13:46 +0300

    test

--- src/test/java/starlight/backend/talent/TalentControllerTest.java
+++ src/test/java/starlight/backend/talent/TalentControllerTest.java
@@ -1,11 +1,15 @@
 package starlight.backend.talent;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TalentControllerTest {
-
+    @Value(value="${server.port}")
+    private int port;
     @Test
     void pagination() {
     }
