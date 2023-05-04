6d438ec67806a7a3b596bf2f1cf4f309c67a302b
tetyaZyna <sv2408sv@gmail.com>
2023-05-04 15:00:46 +0200

    Create a POST endpoint /proofs/{proof-id}/kudos to add new kudos. Fix getKudosOnProof

--- src/main/java/starlight/backend/kudos/controller/impl/KudosControllerImpl.java
+++ src/main/java/starlight/backend/kudos/controller/impl/KudosControllerImpl.java
@@ -1,11 +1,21 @@
 package starlight.backend.kudos.controller.impl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import starlight.backend.exception.ProofNotFoundException;
+import starlight.backend.kudos.KudosService;
+import starlight.backend.kudos.model.response.KudosOnProof;
 
 @AllArgsConstructor
 @Slf4j
@@ -14,24 +24,68 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Kudos", description = "Kudos API")
 @RestController
 public class KudosControllerImpl {
-    /*
     KudosService kudosService;
 
+    @Operation(
+            summary = "Get all kudos on proof",
+            description = "Get all kudos on proof and boolean flag if you as a Sponsor already kudosed this proof"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = ProofNotFoundException.class
+                            )
+                    )
+            )
+    })
     @GetMapping("/proofs/{proof-id}/kudos")
-    public long getKudosOnProof(@PathVariable("proof-id") long proofId) {
+    public KudosOnProof getKudosOnProof(@PathVariable("proof-id") long proofId,
+                                        Authentication auth) {
         log.info("@GetMapping(\"/proofs/{proof-id}/kudos\")");
-
         log.info("Getting proof-id = {}", proofId);
-        return kudosService.getKudosOnProof(proofId);
+        return kudosService.getKudosOnProof(proofId, auth);
     }
 
-    @PreAuthorize("hasRole('TALENT')")
+
+    @Operation(
+            summary = "Add kudos on proof",
+            description = "Add kudos from Sponsor on proof"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = Exception.class
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(
+                                    implementation = Exception.class
+                            )
+                    )
+            )
+    })
     @PostMapping("/proofs/{proof-id}/kudos")
     @ResponseStatus(HttpStatus.CREATED)
-    public void kudosTwo(@PathVariable("proof-id") long proofId, Authentication auth) {
+    public void addKudos(@PathVariable("proof-id") long proofId,
+                         @RequestParam int kudos,
+                         Authentication auth
+    ) {
         log.info("@PostMapping(\"/proofs/{proof-id}/kudos\")");
 
         log.info("Getting proof-id = {}", proofId);
-        kudosService.addKudosOnProof(proofId, auth);
-    }*/
+        kudosService.addKudosOnProof(proofId, kudos, auth);
+    }
 }
--- src/main/resources/db/changelog/dml/005_insert_data_kudos.sql
+++ src/main/resources/db/changelog/dml/005_insert_data_kudos.sql
@@ -1,22 +1,22 @@
 --liquibase formatted sql
 --changeset sasha:inset-kudos-05
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (1,1,2,'2001-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (1,2,3,'2002-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (1,3,4,'2003-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (2,1,2,'2004-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (2,1,2,'2005-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (3,1,2,'2006-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (3,1,2,'2007-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (4,1,2,'2008-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (5,1,2,'2009-09-14 22:11:12');
-INSERT INTO kudos_entity (user_id,proof_id,follower_id,create_data)
-VALUES (6,1,2,'2010-09-14 22:11:12');
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,1,2,'2001-09-14 22:11:12', 50);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,2,3,'2002-09-14 22:11:12', 150);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,3,4,'2003-09-14 22:11:12', 30);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (2,1,2,'2004-09-14 22:11:12', 40);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (2,1,2,'2005-09-14 22:11:12', 70);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,1,2,'2006-09-14 22:11:12', 60);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,1,2,'2007-09-14 22:11:12', 10);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (2,1,2,'2008-09-14 22:11:12', 5);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (2,1,2,'2009-09-14 22:11:12', 56);
+INSERT INTO kudos_entity (sponsor_id,proof_id,follower_id,create_data, count_kudos)
+VALUES (1,1,2,'2010-09-14 22:11:12', 12);
