de7b33c95fb1b5af3a7f99d8c1abd2f61895926d
tetyaZyna <sv2408sv@gmail.com>
2023-05-05 15:06:23 +0200

    Create a Get endpoint /sponsors/{sponsor-id} that return SponsorFullInfo.java

--- src/main/java/starlight/backend/sponsor/service/impl/SponsorServiceImpl.java
+++ src/main/java/starlight/backend/sponsor/service/impl/SponsorServiceImpl.java
@@ -1,14 +1,14 @@
 package starlight.backend.sponsor.service.impl;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import starlight.backend.exception.ProofNotFoundException;
+import starlight.backend.exception.SponsorCanNotSeeAnotherSponsor;
 import starlight.backend.exception.SponsorNotFoundException;
+import starlight.backend.security.service.SecurityServiceInterface;
 import starlight.backend.sponsor.SponsorRepository;
-import starlight.backend.sponsor.model.entity.SponsorEntity;
+import starlight.backend.sponsor.model.response.SponsorFullInfo;
 import starlight.backend.sponsor.model.response.UnusableKudos;
 import starlight.backend.sponsor.service.SponsorServiceInterface;
 
@@ -17,13 +17,29 @@ import starlight.backend.sponsor.service.SponsorServiceInterface;
 @Transactional
 public class SponsorServiceImpl implements SponsorServiceInterface {
     private SponsorRepository sponsorRepository;
-    @PersistenceContext
-    private EntityManager em;
+    private SecurityServiceInterface serviceService;
     @Override
     public UnusableKudos getUnusableKudos(long sponsorId) {
-        if (!sponsorRepository.existsBySponsorId(sponsorId)) {
-            throw new SponsorNotFoundException(sponsorId);
+        var sponsor = sponsorRepository.findById(sponsorId)
+                .orElseThrow(() -> new SponsorNotFoundException(sponsorId));
+        return new UnusableKudos(sponsor.getUnusedKudos());
+    }
+
+    @Override
+    public SponsorFullInfo getSponsorFullInfo(long sponsorId, Authentication auth) {
+        var sponsor = sponsorRepository.findById(sponsorId)
+                .orElseThrow(() -> new SponsorNotFoundException(sponsorId));
+        isItMyAccount(sponsorId, auth);
+        return SponsorFullInfo.builder()
+                .fullName(sponsor.getFullName())
+                .avatar(sponsor.getAvatar())
+                .company(sponsor.getCompany())
+                .build();
+    }
+
+    private void isItMyAccount(long sponsorId, Authentication auth) {
+        if (!serviceService.checkingLoggedAndToken(sponsorId, auth)) {
+            throw new SponsorCanNotSeeAnotherSponsor();
         }
-        return new UnusableKudos(em.find(SponsorEntity.class, sponsorId).getUnusedKudos());
     }
 }
