a2be73606a2d33db6e31cd2d5875fc89efaf7f76
Serhii <104906557+Lion6477@users.noreply.github.com>
2023-05-11 14:34:23 +0200

    Update EmailServiceImpl.java

--- src/main/java/starlight/backend/email/service/impl/EmailServiceImpl.java
+++ src/main/java/starlight/backend/email/service/impl/EmailServiceImpl.java
@@ -63,6 +63,7 @@ public class EmailServiceImpl implements EmailService {
         
         sponsor.setStatus(SponsorStatus.ACTIVE);
         sponsorRepository.save(sponsor);
+//         delayedDeleteEntity.setDeleteDate(null);
         delayDeleteRepository.delete(delayedDeleteEntity);
     }
 
