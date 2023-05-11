39d51051b2b1ac39516bbf16f7284ebf921d1060
Serhii <sekret7d@gmail.com>
2023-05-11 13:46:21 +0200

    add more logs

--- src/main/java/starlight/backend/exception/user/UserNotFoundWithUUIDException.java
+++ src/main/java/starlight/backend/exception/user/UserNotFoundWithUUIDException.java
@@ -8,11 +8,11 @@ import java.util.UUID;
 public class UserNotFoundWithUUIDException extends RuntimeException{
     public UserNotFoundWithUUIDException(UUID uuid) {
         super("User with UUID = " + uuid + "not found");
-        log.debug("User with UUID = " + uuid + " not found in DelayDeleteRepository");
+        log.info("User with UUID = " + uuid + " not found in DelayDeleteRepository");
     }
 
     public UserNotFoundWithUUIDException(String uuid) {
         super("User with UUID = " + uuid + "not found");
-        log.debug("User with UUID = " + uuid + " not found in DelayDeleteRepository");
+        log.info("User with UUID = " + uuid + " not found in DelayDeleteRepository");
     }
 }
