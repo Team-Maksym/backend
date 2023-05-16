10c2d1955d640d1b285212878f3531e5a709a6ec
KateySt <kateystetsenko@gmail.com>
2023-05-16 19:19:21 +0300

    hix

--- src/main/java/starlight/backend/exception/proof/ProofNotFoundException.java
+++ src/main/java/starlight/backend/exception/proof/ProofNotFoundException.java
@@ -1,10 +1,7 @@
 package starlight.backend.exception.proof;
 
-public class ProofNotFoundException extends RuntimeException{
+public class ProofNotFoundException extends RuntimeException {
     public ProofNotFoundException(long id) {
-        super("Proof not found by id " + id);
-    }
-    public ProofNotFoundException(String id) {
         super("Proof not found by id " + id);
     }
 }
