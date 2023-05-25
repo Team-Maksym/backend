230f5c09a17b8c57088b34e4480015cecd9269e0
KateySt <kateystetsenko@gmail.com>
2023-05-25 18:14:45 +0300

    worked ProofControllerV2Test

--- src/test/java/starlight/backend/kudos/controller/KudosControllerTest.java
+++ src/test/java/starlight/backend/kudos/controller/KudosControllerTest.java
@@ -1,20 +1,17 @@
 package starlight.backend.kudos.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
 import starlight.backend.kudos.model.entity.KudosEntity;
@@ -23,32 +20,33 @@ import starlight.backend.kudos.service.KudosServiceInterface;
 
 import java.time.Instant;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = KudosController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(KudosController.class)
 class KudosControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+
     @MockBean
     private Authentication auth;
+
     @MockBean
     private KudosServiceInterface kudosService;
 
-    @DisplayName("JUnit test for getKudosOnProofShouldReturnKudosOnProof")
+    @DisplayName("Test getKudosOnProofShouldReturnKudosOnProof")
     @Test
     @Order(1)
     @WithMockUser(username = "user1", roles = {"SPONSOR"})
-    public void getKudosOnProofShouldReturnKudosOnProof() throws Exception {
-        //Given
+    void getKudosOnProofShouldReturnKudosOnProof() throws Exception {
+        // Given
         int proofId = 1;
         KudosOnProof kudosOnProof = KudosOnProof.builder()
                 .kudosOnProof(1)
@@ -59,7 +57,6 @@ class KudosControllerTest {
 
         // When // Then
         mockMvc.perform(get("/api/v1/proofs/{proof-id}/kudos", proofId))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.kudos_on_proof").value(kudosOnProof.kudosOnProof()))
@@ -68,12 +65,12 @@ class KudosControllerTest {
                 .andExpect(jsonPath("$").isNotEmpty());
     }
 
-    @DisplayName("JUnit test for addKudosShouldReturnKudosEntity")
+    @DisplayName("Test addKudosShouldReturnKudosEntity")
     @Test
     @Order(1)
     @WithMockUser(username = "user1", roles = {"SPONSOR"})
-    public void addKudosShouldReturnKudosEntity() throws Exception {
-        //Given
+    void addKudosShouldReturnKudosEntity() throws Exception {
+        // Given
         int proofId = 1;
         int kudos = 20;
         KudosEntity kudosEntity = KudosEntity.builder()
@@ -83,7 +80,9 @@ class KudosControllerTest {
                 .updateData(Instant.MIN)
                 .createData(Instant.MAX)
                 .build();
-        when(kudosService.addKudosOnProof(proofId, kudos, auth)).thenReturn(kudosEntity);
+        given(kudosService.addKudosOnProof(proofId, kudos, auth)).willReturn(kudosEntity);
+        given(kudosService.addKudosOnProof(eq(proofId), eq(kudos), any(Authentication.class)))
+                .willReturn(kudosEntity);
 
         // When // Then
         mockMvc.perform(post("/api/v1/proofs/{proof-id}/kudos", proofId)
@@ -91,29 +90,31 @@ class KudosControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .characterEncoding("utf-8"))
-                .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.follower_id").value(kudosEntity.getFollowerId()))
                 .andExpect(jsonPath("$.count_kudos").value(kudosEntity.getCountKudos()))
-                .andExpect(jsonPath("$.update_data").value(kudosEntity.getUpdateData()))
-                .andExpect(jsonPath("$.create_data").value(kudosEntity.getCreateData()))
+                .andExpect(jsonPath("$.update_data").value(kudosEntity.getUpdateData().toString()))
+                .andExpect(jsonPath("$.create_data").value(kudosEntity.getCreateData().toString()))
                 .andExpect(jsonPath("$").isNotEmpty());
     }
 
-    @DisplayName("JUnit test for add Kudos Should Return Bad Request When Kudos Parameter Is Not Provided")
+    @DisplayName("Test addKudosShouldReturnBadRequestWhenKudosParameterIsNotProvided")
     @Test
-    public void addKudosShouldReturnBadRequestWhenKudosParameterIsNotProvided() throws Exception {
-        //Given
+    @Order(1)
+    @WithMockUser(username = "user1", roles = {"SPONSOR"})
+    void addKudosShouldReturnBadRequestWhenKudosParameterIsNotProvided() throws Exception {
+        // Given
         int proofId = 1;
-        when(kudosService.getKudosOnProof(proofId, null))
-                .thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+        given(kudosService.getKudosOnProof(proofId, null))
+                .willThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
 
         // When // Then
         mockMvc.perform(post("/api/v1/proofs/{proof-id}/kudos", proofId)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isUnauthorized());
     }
+
 
     @DisplayName("JUnit test for add Kudos Should Return Unauthorized When Kudos Parameter Is Not Authenticated")
     @Test
@@ -122,20 +123,21 @@ class KudosControllerTest {
     public void addKudosShouldReturnUnauthorizedWhenKudosParameterIsNotProvided() throws Exception {
         //Given
         int proofId = 1;
-
+        when(kudosService.addKudosOnProof(proofId, 0, auth))
+                .thenThrow(new ResponseStatusException(HttpStatus.BAD_REQUEST));
         // When // Then
         mockMvc.perform(post("/api/v1/proofs/{proof-id}/kudos", proofId)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 
-    @DisplayName("JUnit test for add Kudos Should Return Unauthorized When User Is Not Authenticated")
+    @DisplayName("Test addKudosShouldReturnUnauthorizedWhenUserIsNotAuthenticated")
     @Test
-    public void addKudosShouldReturnUnauthorizedWhenUserIsNotAuthenticated() throws Exception {
-        //Given
+    void addKudosShouldReturnUnauthorizedWhenUserIsNotAuthenticated() throws Exception {
+        // Given
         int proofId = 1;
-        when(kudosService.addKudosOnProof(proofId, 1, null))
-                .thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
+        given(kudosService.addKudosOnProof(proofId, 1, null))
+                .willThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED));
 
         // When // Then
         mockMvc.perform(post("/api/v1/proofs/{proof-id}/kudos", proofId)
