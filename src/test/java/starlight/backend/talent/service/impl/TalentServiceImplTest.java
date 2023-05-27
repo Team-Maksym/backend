58cdb6a41e57338c404efcfccc72413e53ea5cad
KateySt <kateystetsenko@gmail.com>
2023-05-27 14:02:46 +0300

    TalentServiceImplTest

--- src/test/java/starlight/backend/talent/service/impl/TalentServiceImplTest.java
+++ src/test/java/starlight/backend/talent/service/impl/TalentServiceImplTest.java
@@ -213,7 +213,8 @@ class TalentServiceImplTest {
     @Test
     void deleteTalentProfile() {
         // Given
-      /*  Authentication auth = Mockito.mock(Authentication.class);
+        long talentId = 1L;
+        Authentication auth = Mockito.mock(Authentication.class);
         given(userRepository.findById(user.getUserId())).willReturn(Optional.of(user));
         when(securityService.checkingLoggedAndToken(user.getUserId(), auth)).thenReturn(true);
         Set<ProofEntity> proofList = new HashSet<>();
@@ -224,8 +225,8 @@ class TalentServiceImplTest {
         proofList.add(proof);
         user.setProofs(proofList);
         user.setAuthorities(new HashSet<>(Arrays.asList("TALENT_ROLE")));
-        when(proofRepository.findByUser_UserId(user.getUserId())).thenReturn(Collections.emptyList());
-        doNothing().when(userRepository).deleteById(user.getUserId());
+        when(proofRepository.findByUser_UserId(talentId)).thenReturn(Collections.emptyList());
+        doNothing().when(userRepository).deleteById(talentId);
 
         // When
         talentService.deleteTalentProfile(user.getUserId(), auth);
@@ -233,8 +234,7 @@ class TalentServiceImplTest {
         // Then
         verify(securityService).checkingLoggedAndToken(user.getUserId(), auth);
         verify(userRepository, times(1)).findById(user.getUserId());
-        verify(proofRepository, times(1)).findByUser_UserId(user.getUserId());
-        assertTrue(user.getProofs().isEmpty());*/
+        assertTrue(user.getProofs().isEmpty());
     }
 
     @DisplayName("JUnit test for delete talent method which throw exception Unauthorized")
